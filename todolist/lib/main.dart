bcc1c85105b720e4420e676f490c627db20d1759
andreslowenstein <61715819+andreslowenstein@users.noreply.github.com>
2023-03-06 13:51:21 +0100

    setting up firebase and added some packages

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -16,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: Scaffold(),
     );
   }
 }
