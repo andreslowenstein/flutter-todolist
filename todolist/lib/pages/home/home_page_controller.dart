bb7e6905f10c310fed9bccdf05884eb897221561
andreslowenstein <61715819+andreslowenstein@users.noreply.github.com>
2023-03-07 11:52:53 +0100

    Update home_page_controller.dart

--- todolist/lib/pages/home/home_page_controller.dart
+++ todolist/lib/pages/home/home_page_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todolist/pages/home/cubit/todo_cubit.dart';
@@ -19,7 +20,21 @@ class HomePageController extends StatelessWidget {
           );
         }
         if (state is TodoError) {
-          return Text(state.error);
+          CoolAlert.show(
+            context: context,
+            type: CoolAlertType.success,
+            title: "",
+            widget: Column(
+              children: [
+                Text(
+                  state.error,
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ],
+            ),
+            confirmBtnColor: Theme.of(context).colorScheme.primary,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+          );
         }
         state as TodoLoaded;
         return HomePage(
